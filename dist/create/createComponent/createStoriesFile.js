021ec567060f56dbbbfce357e210d7ab028f74e8
Max Kudla <maksim.kudla@gmail.com>
2019-08-07 11:01:19 +0300

    v0.0.8

--- dist/create/createComponent/createStoriesFile.js
+++ dist/create/createComponent/createStoriesFile.js
@@ -13,9 +13,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const handlebars_1 = __importDefault(require("handlebars"));
 const fs = __importStar(require("fs"));
 const path_1 = __importDefault(require("path"));
-const capitalize_1 = __importDefault(require("../../utils/capitalize"));
 exports.default = (args, componentPath) => {
-    const filepath = path_1.default.join(__dirname, "../..", 'templates', args.functional ? 'FunctionComponent.hbs' : 'ClassComponent.hbs');
+    const filepath = path_1.default.join(__dirname, "../..", "templates", "ComponentStories.hbs");
     const template = fs.readFileSync(filepath, 'utf8');
-    fs.writeFileSync(path_1.default.join(componentPath, `${capitalize_1.default(args.name)}.tsx`), handlebars_1.default.compile(template)(args));
+    fs.writeFileSync(path_1.default.join(componentPath, `stories.tsx`), handlebars_1.default.compile(template)(args));
 };
--- dist/create/createComponent/index.js
+++ dist/create/createComponent/index.js
@@ -16,6 +16,7 @@ const capitalize_1 = __importDefault(require("../../utils/capitalize"));
 const createComponentFile_1 = __importDefault(require("./createComponentFile"));
 const createTypesFile_1 = __importDefault(require("./createTypesFile"));
 const createIndexFile_1 = __importDefault(require("./createIndexFile"));
+const createStoriesFile_1 = __importDefault(require("./createStoriesFile"));
 const createComponent = (args) => {
     const dirPath = args.global && args.componentsPath ? path.join(args.rootPath, args.componentsPath) : "";
     const componentPath = path.join(dirPath, capitalize_1.default(args.name));
@@ -30,6 +31,9 @@ const createComponent = (args) => {
         createComponentFile_1.default(args, componentPath);
         createTypesFile_1.default(args, componentPath);
         createIndexFile_1.default(args, componentPath);
+        if (args.stories) {
+            createStoriesFile_1.default(args, componentPath);
+        }
     }
 };
 exports.default = createComponent;
